3b2222450bf76c71e52823ad33da2c7e0c95dca9
TingboW <104877939+TingboW@users.noreply.github.com>
2022-09-04 16:55:36 -0700

    lab

--- shopcart/src/App.js
+++ shopcart/src/App.js
@@ -6,25 +6,32 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function App() {
+function Nav(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="navbar">
+      <h1>
+        <span>Shop to React </span> </h1> 
     </div>
-  );
+        <FontAwesomeIcon icon={faShoppingCart} />
+  )
 }
+
+     // <header className="App-header">
+    //  <img src={logo} className="App-logo" alt="logo" />
+    // <p>
+    //  Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //      <a
+     //       className="App-link"
+     //       href="https://reactjs.org"
+      //      target="_blank"
+      //      rel="noopener noreferrer"
+      //    >
+       //     Learn React
+      //    </a>
+      //  </header>
+     // </div></>
+//  );
+// }
 
 export default App;
