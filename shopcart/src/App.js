cbb02539553339d4590f0b2ba69a344ec6176a67
TingboW <104877939+TingboW@users.noreply.github.com>
2022-09-10 21:05:39 -0700

    w2 lab assignment

--- shopcart/src/App.js
+++ shopcart/src/App.js
@@ -1,104 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
-
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-//import { products } from "./products";
-//import Nav from "./navigation";
+//import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { products } from "./products";
+import Navbar from "./navbar";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      ProductList: [
-        {
-          id: 1,
-          title: "Unisex Cologne",
-          quantity: 0,
-          img: "/products/cologne.jpg",
-          done: false
-        },
-        { id: 2, 
-          title: "Apple iWatch", 
-          quantity: 0, 
-          img: "/products/iwatch.jpg",
-          done: false },
-        {
-          id: 3,
-          title: "Unique Mug",
-          quantity: 0,
-          img: "/products/mug.jpg",
-          done: false
-        },
-        {
-          id: 4,
-          title: "Mens Wallet",
-          quantity: 0,
-          img: "/products/wallet.jpg",
-          done: false
-        }
-      ],
-    }
+      products: products,
+    };
   }
 
+  increaseBtn = (addvalue) => {
+    if (addvalue.quantity < 10) {
+      const updateValue = addvalue.quantity++;
+      this.setState({ updateValue });
+    }
+  };
+
+  decreaseBtn = (subvalue) => {
+    if (subvalue.quantity > 0) {
+      const updateValue = subvalue.quantity--;
+      this.setState({ updateValue });
+    }
+  };
 
 render () {
-  let item;
-  let itemCount;
-  item = <Items itemlists={this.state.ProductList} />;
-
-  itemCount = <Totals counts={this.state.ProductList} />;
-
   return (
-    {},
-    (
-      <div className="App">
-        <div className="navbar p-5 bg-info">
-        <h1>
-          <span className="px-2">
-          Shop to React </span>
-          </h1>
-
-          <div>
-        <FontAwesomeIcon icon={faShoppingCart} /> 
-        <span className=""> {itemCount}</span>
-       </div>
-        </div>
-        
-        {item}
+    { /* Output final view */},
+    <div className="App">
+    <Navbar
+          ItemCount={this.state.products
+            .map((prod) => prod.quantity)
+            .reduce((acc, curr) => acc + curr, 0)}
+          prods={this.state.products}
+          increaseBtn={this.increaseBtn}
+          decreaseBtn={this.decreaseBtn}
+   />
       </div>
-    )
-  );
-}
-}
-
-function Totals (props) {
-  let total = 0;
-
-  total = props.counts.map(counts => counts.quantity).reduce((prev, next) => prev + next);
-  
-  return (
-    <div> {total} items </div>
-
-  )
-}
-
-function Items(props) {
-  return (
-    <div>
-    {props.itemlists.map((ProductList) => (
-      <div id="items">
-      <h3 tag="a" href="#" className="mx-5">{ProductList.title}</h3>
-      <img src={ProductList.img} width="150" />
-      <span className="mx-5 p-3 border border-3">
-      {ProductList.quantity}</span> quantity
-      </div>
-    ))}
-     </div>
-  );
+    );
+  }
 }
 
 export default App;
--- shopcart/src/products.js
+++ shopcart/src/products.js
@@ -1,26 +1,31 @@
 export const products = [
     { 
-        id: 1, 
-        image: './products/cologne.jpg', 
-        desc: 'Unisex Cologne', 
-        value: 0 
+        id: 1,
+        title: "Unisex Cologne",
+        quantity: 0,
+        img: "/products/cologne.jpg",
+        ratings: 4.3,
     },
     { 
         id: 2, 
-        image: './products/iwatch.jpg',
-        desc: 'Apple iWatch', 
-        value: 0 
+        title: "Apple iWatch", 
+        quantity: 0, 
+        img: "/products/iwatch.jpg",
+        ratings: 3.5,
     },
     { 
-        id: 3, 
-        image: './products/mug.jpg', 
-        desc: 'Unique Mug', 
-        value: 0 
+        id: 3,
+        title: "Unique Mug",
+        quantity: 0,
+        img: "/products/mug.jpg",
+        ratings: 3.8,
     },
     { 
-        id: 4, 
-        image: './products/wallet.jpg', 
-        desc: 'Mens Wallet', 
-        value: 0 
+        id: 4,
+        title: "Mens Wallet",
+        quantity: 0,
+        img: "/products/wallet.jpg",
+        ratings: 4.1,
     }
 ]
+export default products;
